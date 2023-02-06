5f138d8fa1ad87ba59852193302c0018c5b3832a
Mateo Pérez de Gracia <mperezdegracia@itba.edu.ar>
2023-02-06 13:05:50 -0300

    slam bounce

--- the-mallet/Assets/Scripts/PlayerMovement.cs
+++ the-mallet/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,12 @@ public class PlayerMovement : MonoBehaviour
     public float jumpStrength = 14f;
     public float moveSpeed = 7f;
     public float slamStrength = 25f;
+    public float slamBounceMultiplier = 1.3f; 
 
     private Rigidbody2D rb; // Rigidbody
     private BoxCollider2D coll; // Collider
     private bool onGround; // Stores whether player is on the ground
     private bool usingSlam; // Stores whether slam has been activated
-
     [SerializeField] private LayerMask jumpableGround; 
 
 
@@ -21,6 +21,7 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
+
 
     }
 
@@ -33,18 +34,11 @@ public class PlayerMovement : MonoBehaviour
             float dirX = Input.GetAxisRaw("Horizontal");
             rb.velocity = new Vector2(moveSpeed * dirX, rb.velocity.y);
         }
-        
-        // Checks if the player is grounded and turns off the slam if it is
-        if (onGround = IsGrounded())
-        {
-            usingSlam = false;
-        }
-
-
+        onGround = IsGrounded();
         // If jump is pressed
         if (Input.GetButtonDown("Jump") && onGround)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpStrength);
+            Jump();
         } 
         // If the player is on the air then the slam is activated
         else if (usingSlam || Input.GetButtonDown("Jump") && !onGround)
@@ -55,10 +49,33 @@ public class PlayerMovement : MonoBehaviour
 
         
     }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Platform tile =  collision.gameObject.GetComponent<Platform>();
+        if (tile && usingSlam)  // TODO : usingSlam is always false on collision due to Update() 
+        {
+            usingSlam = false; // note patch
+            tile.SlamHit();
+            Bounce();
+            // jump ?)
+        }
+    }
 
     // Checks if player is touching ground
     private bool IsGrounded() 
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpStrength);
+
+    }
+    private void Bounce()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpStrength * slamBounceMultiplier);
+
     }
 }
